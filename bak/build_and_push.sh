81a9f97cf7983a80b0bc7d6187403cd1c91fe10c
pangyuteng <pangyuteng@gmail.com>
2026-06-12 22:14:51 +0000

    wip

--- bak/build_and_push.sh
+++
@@ -1,5 +0,0 @@
-docker build -t pangyuteng/nfs-server -f Dockerfile.nfs-server .
-docker push pangyuteng/nfs-server
-
-docker build -t pangyuteng/httpbin-nfs -f Dockerfile.httpbin .
-docker push pangyuteng/httpbin-nfs 
